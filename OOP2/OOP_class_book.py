dc7c5fa696dbb246158e150edbf50ab8d8bac32a
madaz1movsa <kutbidinovdanislan9@gmail.com>
2024-05-29 15:41:31 +0600

    Создания Класса Книга

--- OOP2/OOP_class_book.py
+++ OOP2/OOP_class_book.py
@@ -10,19 +10,24 @@ class Book:
         print(self.names, self.author, self.pages, self.yearswriting, self.contents)
 
 
-Book1 = Book(names_pm="NAME =  The Book thief", author_pm="Marcuz Suzak", pages_pm=5343, yearswriting_pm= "This Book writing of 2005 years",
+Book1 = Book(names_pm="NAME =  The Book thief", author_pm="Marcuz Suzak", pages_pm=5343,
+             yearswriting_pm= "This Book writing of 2005 years",
              contents_pm="История о немецкой девочке по имени Лизель")
 
 print(Book1.names)
 
-Bokok2 = Book(names_pm="NAME =  От Нуля к единице", author_pm="Piter Till", pages_pm=182, yearswriting_pm="16 - September, 2014 - yers",
+Bokok2 = Book(names_pm="NAME =  От Нуля к единице", author_pm="Piter Till", pages_pm=182,
+              yearswriting_pm="16 - September, 2014 - yers",
               contents_pm="A future Progress")
+
 print(Bokok2.names)
 
 Book3 = Book(names_pm="NAME =  Сымый Богатый Человек в Вавилоне", author_pm=" George Kleison", pages_pm=181,
              yearswriting_pm=1920, contents_pm="Achieve financial independence")
+
 print(Book3.names)
 
 Book4 =Book(names_pm="NAME = 'Prince of the Siberian Kyrgyz'", author_pm="'В.И. Козодой'", pages_pm=185, yearswriting_pm= "Novosibirsk, 23 - November!",
             contents_pm="История Сибирских Кыргызов!")
+
 print(Book4.names)
