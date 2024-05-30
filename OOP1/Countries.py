0c5f77da483637a23c26f797c5532a550140bda6
madaz1movsa <kutbidinovdanislan9@gmail.com>
2024-05-30 08:22:31 +0600

    Практическая Задача с Классами

--- OOP1/Countries.py
+++ OOP1/Countries.py
@@ -7,10 +7,10 @@ class Country:
         self.Language = Language_pm
 
 
-Kyrgyzstan = Country(population_pm="Kyrgyzstan - Population = 7 000_000", Climat_pm="continental!",
+Kyrgyzstan = Country(population_pm="Kyrgyzstan - Population = 7 000_000", Climat_pm="continental",
                      Locationcountry_pm="Central Asia",
                      Themonetarycurrencyoftheod_pm="SOM",
-                     Language_pm="National Kyrgyz language!")
+                     Language_pm="National Kyrgyz language")
 
 Country1 = Kyrgyzstan.populataion
 print(Country1)
@@ -29,7 +29,7 @@ print(Country2)
 
 China = Country(population_pm="China - Population = 2_334_344_323_322", Climat_pm="climate is continental with cold, dry winters and hot, humid summers.",
                 Locationcountry_pm="China is Located in 'East Asia.'",
-                Themonetarycurrencyoftheod_pm="Oficial Valute in China,this 'Юань'!",
+                Themonetarycurrencyoftheod_pm="Oficial Valute in China,this 'Юань'",
                 Language_pm="Language in China 'Language China!")
 
 Country3 = China.populataion
